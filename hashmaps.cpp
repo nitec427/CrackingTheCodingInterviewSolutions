630b0ef91f9f78109b06d74db650b4d01c4df970
nitec_427 <nitec_427@hotmail.com>
2022-08-16 17:00:30 +0300

    Cracking the coding interview started

---
+++ hashmaps.cpp
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <iostream>
+using namespace std; 
+
+class Node{
+// State your intend no matter what
+private:
+    int data;
+    Node* left, *right;
+    bool color;
+public:
+    Node(int);
+};
+
+Node::Node(int input){
+    left = right = parent = nullptr;
+    data = input;
+}
+
+class RedBlackTree{
+
+    
+    
+}
+
+
+int main(){
+
+}
